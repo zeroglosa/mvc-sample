a6858b2d6ff384bc1165a40887f9176ba5552d48
cleydsonjr <cleydson.fibonacci@solucoesageis.com.br>
2015-12-03 16:54:02 -0200

    Inserindo teste com empate

--- src/test/groovy/br/com/zg/trainning/CampeonatoAppSpec.groovy
+++ src/test/groovy/br/com/zg/trainning/CampeonatoAppSpec.groovy
@@ -4,7 +4,7 @@ import spock.lang.Specification
 
 class CampeonatoAppSpec extends Specification {
 
-    void 'Aplicativo deve retornar o campeao do campeonato'() {
+    void 'Aplicativo deve retornar o campeao do campeonato quando executar operação 1'() {
         setup:
         ByteArrayOutputStream outputStream = new ByteArrayOutputStream()
         System.out = new PrintStream(outputStream)
@@ -17,6 +17,22 @@ class CampeonatoAppSpec extends Specification {
         outputStream.toString().readLines().last().contains "Corinthians"
 
         where:
-        input = this.class.getResourceAsStream('/input1.txt')
+        input = this.class.getResourceAsStream('/inputComCampeao.txt')
+    }
+
+    void 'Aplicativo deve retornar o lanterna do campeonato quando executar operação 2'() {
+        setup:
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream()
+        System.out = new PrintStream(outputStream)
+        System.in = input
+
+        when:
+        println 'Executar logica do controller inicial'
+
+        then:
+        outputStream.toString().readLines().last().contains "Grêmio"
+
+        where:
+        input = this.class.getResourceAsStream('/inputComEmpate.txt')
     }
 }
