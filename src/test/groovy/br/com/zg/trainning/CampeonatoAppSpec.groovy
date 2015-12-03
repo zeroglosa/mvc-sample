ef15c4a1f208157758d419e714555d8e7283223a
cleydsonjr <cleydson.fibonacci@solucoesageis.com.br>
2015-12-03 15:40:57 -0200

    Inserindo primeiro teste

--- src/test/groovy/br/com/zg/trainning/CampeonatoAppSpec.groovy
+++ src/test/groovy/br/com/zg/trainning/CampeonatoAppSpec.groovy
@@ -7,14 +7,16 @@ class CampeonatoAppSpec extends Specification {
 
     void 'Aplicativo deve retornar o campeao do campeonato'() {
         setup:
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream()
+        System.out = new PrintStream(outputStream)
         System.in = input
 
         when:
-        // CampeonatoController controller = new CampeonatoController()
-        // controller.inicieCampeonato()
+//        CampeonatoController controller = new CampeonatoController()
+//        controller.inicieCampeonato()
 
         then:
-        // System.ou. // lastLine Corinthias
+        outputStream.toString().readLines().last() == "Corinthians"
 
         where:
         input = this.class.getResourceAsStream('/input1.txt')
