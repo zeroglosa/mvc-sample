50b39a7bef4dfc5487c361a77d793f8b0c068204
Luiz Henrique <luizhenrique321@gmail.com>
2016-04-07 11:45:57 -0300

    #2 - Criação da classe Campeonato no modelo, de acordo com a abstração de Campeonato descrita no requisito.

---
+++ src/main/groovy/br/com/zg/trainning/model/Campeonato.groovy
@@ -0,0 +1,12 @@
+package br.com.zg.trainning.model
+
+/**
+ * Created by luizhenrique on 07/04/16.
+ */
+class Campeonato {
+
+	String nome
+	int anoCampeonato
+	int quantidadeRodadas
+	List<Time> timesParticipantes
+}
